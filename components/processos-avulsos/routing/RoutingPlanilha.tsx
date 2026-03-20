834243ce6bbc308907c4e213475034fa7ee3d88b
psilas85 <psilas_santos@yahoo.com.br>
2026-03-20 20:01:09 +0000

    link download template roteirizacao

--- components/processos-avulsos/routing/RoutingPlanilha.tsx
+++ components/processos-avulsos/routing/RoutingPlanilha.tsx
@@ -241,8 +241,9 @@ export default function RoutingPlanilha() {
         </button>
 
         <a
-          href="/templates/template_routing.xlsx"
+          href="/templates/roteirizacao_operacional_upload.xlsx"
           target="_blank"
+          title="Baixar modelo padrão (XLSX) para roteirização"
           className="px-4 py-2 border rounded text-sm text-blue-600 border-blue-200 hover:bg-blue-50"
         >
           Baixar template
